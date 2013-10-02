7234fff8dd0ee4bc914abb2f0631f556031f2387
Armaghan Saqib <ar.saqib@gmail.com>
2013-10-02 05:19:04 +0000

    Extra files

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm munshi9.sql
+rm munshi9.sql.zip
+wget http://mavsol.com/munshi9/munshi9.sql.zip
+unzip munshi9.sql.zip
+sqlplus munshi8/gbaba4000 @munshi9.sql
---
+++ genbackup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export ORACLE_HOME=/usr/lib/oracle/xe/app/oracle/product/10.2.0/server
+export ORACLE_SID=XE
+mkdir /root/backups/
+cd /root/backups
+/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/sqlplus munshi8/gbaba4000 @genbackup.sql
+/bin/bash /root/backups/dobackup.sh

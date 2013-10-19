2308d226e0a0a645e925084c3a1dd617dfd59127
Armaghan Saqib <ar.saqib@gmail.com>
2013-10-19 07:44:37 +0500

    Changes for virtual machine

--- genbackup.sh
+++ genbackup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export ORACLE_HOME=/usr/lib/oracle/xe/app/oracle/product/10.2.0/server
 export ORACLE_SID=XE
-mkdir /root/backups/
-cd /root/backups
-/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/sqlplus munshi8/gbaba4000 @/var/www/munshi9/genbackup.sql
-/bin/bash /root/backups/dobackup.sh
+mkdir -p /var/www/lighttpd/munshi9/backups/
+cd /var/www/lighttpd/munshi9/backups/
+/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/sqlplus munshi8/gbaba4000 @/var/www/lighttpd/munshi9/genbackup.sql
+/bin/bash /var/www/lighttpd/munshi9/backups/dobackup.sh

2a2f06c4cddc6d0a578109b915e76fe848e404f7
Armaghan Saqib <ar.saqib@gmail.com>
2013-11-18 06:47:26 +0500

    Corrected invoices printing bug

--- reports.pl
+++ reports.pl
@@ -126,10 +126,10 @@ sub invoice {
     $vars->{hdr}     = $dbs->query( 'SELECT * FROM hc_invoices WHERE inv_num=?', $id )->hash;
     $vars->{company} = $dbs->query( 'SELECT * FROM hc_companies WHERE comp_code=?', $vars->{hdr}->{comp_code} )->hash;
     $vars->{dtl}     = $dbs->query( "
-	SELECT to_char(charge_date, 'yymmdd') id, tr_num, charge_date, res_id, guest_name1, other_ref1, other_ref2, room_num, 0-amount amount, ROUND((0-amount) / 1.16, 2) tax_amt
+	SELECT to_char(charge_date, 'yymmdd')||tr_num id, tr_num, charge_date, res_id, guest_name1, other_ref1, other_ref2, room_num, 0-amount amount, ROUND((0-amount) / 1.16, 2) tax_amt
 	FROM hc_charges WHERE bill_num=? AND sale_id = 0 ORDER BY 1", $id )->map_hashes('id');
     $vars->{chq} = $dbs->query( "
-	SELECT to_char(sale_date, 'yymmdd') id, hc_fb_sale.* FROM hc_fb_sale
+	SELECT to_char(sale_date, 'yymmdd')||sale_id id, hc_fb_sale.* FROM hc_fb_sale
 	WHERE sale_id IN (SELECT sale_id FROM hc_charges WHERE bill_num = ?) ORDER BY 1", $id )->map_hashes('id');
     print $q->header();
     $tt->process( "$tmpl.tmpl", $vars ) || die $tt->error(), "\n";

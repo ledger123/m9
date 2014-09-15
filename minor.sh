a6178b063d46cbc32eba6989353372113d6349db
Armaghan Saqib <ar.saqib@gmail.com>
2014-09-15 15:13:30 +0500

    Minor\

--- minor.sh
+++ minor.sh
@@ -2,4 +2,4 @@
 export ORACLE_HOME=/usr/lib/oracle/xe/app/oracle/product/10.2.0/server
 export ORACLE_SID=XE
 cd /var/www/lighttpd/munshi9
-/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/sqlplus munshi8/gbaba4000 @http://mavsol.com/munshi9/minor.sql
+/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/sqlplus munshi8/gbaba4000 @http://www.mavsol.com/munshi9/minor.sql

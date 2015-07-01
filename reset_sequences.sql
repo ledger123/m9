f123f583a5dcf220d6e6eba2f3eddd8386f32d7c
Armaghan Saqib <ar.saqib@gmail.com>
2015-07-01 09:42:47 +0500

    Reset sequences

---
+++ reset_sequences.sql
@@ -0,0 +1,144 @@
+DROP SEQUENCE event;
+CREATE SEQUENCE event START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE hc_companies_visits_id;
+CREATE SEQUENCE hc_companies_visits_id START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE hc_fb_recipies_id;
+CREATE SEQUENCE hc_fb_recipies_id START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE hc_fb_sale_lines_id;
+CREATE SEQUENCE hc_fb_sale_lines_id START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE seq_adv_dtl_tr_num;
+CREATE SEQUENCE seq_adv_dtl_tr_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE seq_adv_tr_num;
+CREATE SEQUENCE seq_adv_tr_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE seq_banquet;
+CREATE SEQUENCE seq_banquet START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE seq_charges;
+CREATE SEQUENCE seq_charges START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE seq_ec_points;
+CREATE SEQUENCE seq_ec_points START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE seq_ec_tr_num;
+CREATE SEQUENCE seq_ec_tr_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE seq_fb_credits;
+CREATE SEQUENCE seq_fb_credits START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE seq_fb_sale;
+CREATE SEQUENCE seq_fb_sale START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE seq_hc_inv_num;
+CREATE SEQUENCE seq_hc_inv_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE seq_hk_activity;
+CREATE SEQUENCE seq_hk_activity START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE seq_ic_pr;
+CREATE SEQUENCE seq_ic_pr START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE seq_po_num_nff;
+CREATE SEQUENCE seq_po_num_nff START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE seq_rec_tr_num;
+CREATE SEQUENCE seq_rec_tr_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE seq_res;
+CREATE SEQUENCE seq_res START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_ap_bank_reco_tr_num;
+CREATE SEQUENCE s_ap_bank_reco_tr_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_ap_header_inv_id;
+CREATE SEQUENCE s_ap_header_inv_id START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_ap_header_recu_inv_id;
+CREATE SEQUENCE s_ap_header_recu_inv_id START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_ap_payments_tmp_tr_num;
+CREATE SEQUENCE s_ap_payments_tmp_tr_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_ap_payments_tr_num;
+CREATE SEQUENCE s_ap_payments_tr_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_ar_header_inv_num;
+CREATE SEQUENCE s_ar_header_inv_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_ar_header_recu_inv_num;
+CREATE SEQUENCE s_ar_header_recu_inv_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_ar_receipts_receipt_num;
+CREATE SEQUENCE s_ar_receipts_receipt_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_fa_trans_tr_id;
+CREATE SEQUENCE s_fa_trans_tr_id START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_gl_charge_trans_tr_id;
+CREATE SEQUENCE s_gl_charge_trans_tr_id START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_gl_conv_rates_tr_num;
+CREATE SEQUENCE s_gl_conv_rates_tr_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_gl_je_batches_batch_num;
+CREATE SEQUENCE s_gl_je_batches_batch_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_gl_je_header_je_id;
+CREATE SEQUENCE s_gl_je_header_je_id START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_gl_journal_tr_num;
+CREATE SEQUENCE s_gl_journal_tr_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_hc_fb_sale_sale_id;
+CREATE SEQUENCE s_hc_fb_sale_sale_id START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_hc_guests_guest_id;
+CREATE SEQUENCE s_hc_guests_guest_id START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_hc_res_res_id;
+CREATE SEQUENCE s_hc_res_res_id START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_hc_trans_detail_tr_num;
+CREATE SEQUENCE s_hc_trans_detail_tr_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_hc_trans_tr_num;
+CREATE SEQUENCE s_hc_trans_tr_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_ic_costs_cost_id;
+CREATE SEQUENCE s_ic_costs_cost_id START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_ic_trans_header_tmp_tr_num;
+CREATE SEQUENCE s_ic_trans_header_tmp_tr_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_ic_trans_tr_num;
+CREATE SEQUENCE s_ic_trans_tr_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_po_header_blank_po_num;
+CREATE SEQUENCE s_po_header_blank_po_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_po_header_po_num;
+CREATE SEQUENCE s_po_header_po_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_po_header_recu_po_num;
+CREATE SEQUENCE s_po_header_recu_po_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_so_comm_header_tr_num;
+CREATE SEQUENCE s_so_comm_header_tr_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_so_header_blank_so_num;
+CREATE SEQUENCE s_so_header_blank_so_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_so_header_recu_so_num;
+CREATE SEQUENCE s_so_header_recu_so_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE s_so_header_so_num;
+CREATE SEQUENCE s_so_header_so_num START WITH 1 ORDER NOCACHE;
+
+DROP SEQUENCE uniqueseq;
+CREATE SEQUENCE uniqueseq START WITH 1 ORDER NOCACHE;
+
